f23f9c6f1a5ae84a4d04701df5c712b2fbe903c0
wb_liubeiluo <wb_liubeiluo@kuaishou.com>
2025-04-17 21:17:58 +0800

    add 添加生命周期函数

--- pkg/excelx/unmarshal_test.go
+++ pkg/excelx/unmarshal_test.go
@@ -1,7 +1,6 @@
 package excelx
 
 import (
-	"auth_delete/model"
 	"fmt"
 	"testing"
 	"time"
@@ -14,6 +13,29 @@ type User struct {
 	Sex  *string `excel:"sex"`
 }
 
+type Access struct {
+	Id             int    `excel:"id"`
+	ProductPid     string `excel:"product_pid"`
+	SubjectType    int    `excel:"subject_type"`
+	Subject        string `excel:"subject"`
+	Username       string `excel:"username"`
+	Dept1          string `excel:"dept1"`
+	Dept1Code      string `excel:"dept1_code"`
+	Department     string `excel:"department"`
+	DepartmentCode string `excel:"department_code"`
+	Status         string `excel:"离职状态"`
+	ObjectType     int    `excel:"object_type"`
+	Object         string `excel:"object"`
+	RoleRid        string `excel:"role_rid"`
+	RoleProductPid string `excel:"role_product_pid"`
+	CreatedAt      string `excel:"created_at"`
+	UpdatedAt      string `excel:"updated_at"`
+	ValidityType   int    `excel:"validity_type"`
+	BeginTime      string `excel:"begin_time"`
+	EndTime        string `excel:"end_time"`
+	IsDeleted      int    `excel:"is_deleted"`
+}
+
 func TestName(t *testing.T) {
 	var users []User
 	err := Unmarshal("./测试.xlsx", &users)
@@ -24,7 +46,7 @@ func TestName(t *testing.T) {
 }
 
 func TestName1(t *testing.T) {
-	var access []model.Access
+	var access []Access
 	now := time.Now()
 	err := Unmarshal("./海外节点权限回收信息列表.xlsx", &access, "离职人员权限清理")
 	if err != nil {
