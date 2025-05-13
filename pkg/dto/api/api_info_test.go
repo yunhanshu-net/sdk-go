8d990e5e6c9b6205c934ef066e5e984f8984ee1f
wb_liubeiluo <wb_liubeiluo@kuaishou.com>
2025-05-13 13:31:49 +0800

    fix: widget

--- pkg/dto/api/api_info_test.go
+++ pkg/dto/api/api_info_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 type AddReq struct {
-	A        int    `json:"a" form:"a" runner:"code:a;name:值a;required;type:number;example:100;placeholder:请输入值a" validate:"required,min=-1000,max=10000"`
-	B        int    `json:"b" form:"b" runner:"code:b;name:值b;required;type:number;example:200;placeholder:请输入值b" validate:"required,min=-1000,max=10000"`
+	A        int    `json:"a" form:"a" runner:"code:a;name:值a;type:number;example:100;placeholder:请输入值a" validate:"required,min=-1000,max=10000"`
+	B        int    `json:"b" form:"b" runner:"code:b;name:值b;type:number;example:200;placeholder:请输入值b" validate:"required,min=-1000,max=10000"`
 	Receiver string `json:"receiver" form:"receiver" runner:"code:receiver;name:接收人;widget:select;default_value:beiluo;options:admin,beiluo,user;type:string;placeholder:请输入接收人"`
 	Desc     string `json:"desc" form:"desc" runner:"code:desc;name:描述;type:string;placeholder:请描述此次计算;callback:OnInputFuzzy"`
 }
